d4ca4ca3b2b07afe1865891fe6f60f50b23d8e2a
cjysy0104 <cjysy0104@gmail.com>
2025-08-29 17:20:43 +0900

    0829

---
+++ 99_HomeWork/src/com/kh/hw1/plant/run/PlantCreateRun.java
@@ -0,0 +1,10 @@
+package com.kh.hw1.plant.run;
+
+public class PlantCreateRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 99_HomeWork/src/com/kh/hw1/plant/run/PlantRun.java
@@ -0,0 +1,8 @@
+package com.kh.hw1.plant.run;
+
+public class PlantRun {
+
+	public static void main(String[] args) {
+		
+	}
+}
